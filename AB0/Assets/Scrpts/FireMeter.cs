8c0765f8fbae65200069fe497c0f5a7296f15651
ABbuff <adambaer@buffalo.edu>
2022-12-17 10:54:44 -0500

    Added FireMeter script

--- AB0/Assets/Scrpts/FireMeter.cs
+++ AB0/Assets/Scrpts/FireMeter.cs
@@ -2,19 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MechControlCode : MonoBehaviour
+public class FireMeter : MonoBehaviour
 {
-    public double score;
-    
+    public MechControlCode MechanicalController;
+    private bool needsChange;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate()
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (needsChange)
+        {
+            private int
+            while()
+        }
     }
 }
