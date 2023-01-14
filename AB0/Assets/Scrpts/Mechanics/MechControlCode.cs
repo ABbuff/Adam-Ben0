e1a9455e45e47026b75360b9f38a96735277e192
ABbuff <adambaer@buffalo.edu>
2023-01-14 17:17:56 -0500

    added objectBios

--- AB0/Assets/Scrpts/Mechanics/MechControlCode.cs
+++ AB0/Assets/Scrpts/Mechanics/MechControlCode.cs
@@ -6,22 +6,39 @@ public class MechControlCode : MonoBehaviour
 {
     public float score;
     public float maxScore;
+    private GameObject[] burnable;
+    private int maxHealth, totalBurnt;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        findAllBurnable();
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        //Calculate the score by dividing the  total burntHealth  by the  total maxHealth 
+        for (int i=0;i<burnable.Length;i++){
+            score += (burnable[i].getComponent<ObjectBio>().getHealth) - 100;
+        }
+    } 
 
     public void outOfTime()
     {
         // display end game screen for Fighter victory
 
+    }
+
+    //HELPER METHODS
+    public void findAllBurnable(){
+        burnable = GameObject.FindObjectsWithTag("burnable");
+        maxScore = 100 * burnable.Length;
+    }
+    
+    //ACCESSORS
+    public int getMaxScore(){
+        return (float) maxHealth;
     }
 }
